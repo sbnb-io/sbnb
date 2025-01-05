8f438941b3ce7dbfb9c6a6eb7b0eebad735e9bb5
Abylay Ospan <aospan@sbnb.io>
2025-01-05 01:42:53 +0000

    dev-env: add docker, qemu, etc

--- board/sbnb/sbnb/rootfs-overlay/usr/sbin/_sbnb-dev-env-container.sh
+++ board/sbnb/sbnb/rootfs-overlay/usr/sbin/_sbnb-dev-env-container.sh
@@ -13,7 +13,8 @@ apt-get update -y \
         dosfstools git apt-file systemd net-tools \
         sudo pciutils parted fio nvme-cli smartmontools \
         ipmitool yamllint jq sysstat strace curl libelf-dev ncurses-dev \
-        exuberant-ctags iputils-ping dnsutils mc docker.io
+        exuberant-ctags iputils-ping dnsutils mc docker.io qemu-kvm ovmf \
+        genisoimage
 
 # Finally start tmux for fellow developers
 tmux new-session -A -s main
