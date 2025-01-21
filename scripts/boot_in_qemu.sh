4b6d94b066b951cc92e7afdc85f9860cebb1c764
Abylay Ospan <aospan@sbnb.io>
2025-01-21 14:56:59 +0000

    Improve boot in qemu script
    - copy raw image to avoid locking issues
    - start VNC :0 (5900)

--- scripts/boot_in_qemu.sh
+++ scripts/boot_in_qemu.sh
@@ -3,7 +3,9 @@
 set -euxo pipefail
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-IMG_FILE="${SCRIPT_DIR}"/../buildroot/output/images/sbnb.raw
+IMG_FILE_SRC="${SCRIPT_DIR}"/../buildroot/output/images/sbnb.raw
+IMG_FILE="${SCRIPT_DIR}"/../buildroot/output/images/sbnb-qemu.raw
+cp ${IMG_FILE_SRC} ${IMG_FILE}
 
 # Start QEMU with OVMF (uefi) BIOS and supply sbnb image as boot disk
 qemu-system-x86_64 \
@@ -12,4 +14,5 @@ qemu-system-x86_64 \
         -cpu host \
         -accel kvm -m 16G -smp 2 \
         -bios /usr/share/ovmf/OVMF.fd \
+        -vnc :0 \
         -hda ${IMG_FILE}
