3679310d0c8a2205f80a0a9e5020db8b68374389
Abylay Ospan <aospan@sbnb.io>
2025-02-02 16:55:06 +0000

    Mount efivarfs to access UEFI variables

--- board/sbnb/sbnb/post_build.sh
+++ board/sbnb/sbnb/post_build.sh
@@ -8,3 +8,10 @@ IMAGE_VERSION="${DATE}-${RUN_NUMBER}"
 OS_RELEASE="${TARGET_DIR}/etc/os-release"
 echo "IMAGE_ID=sbnb-linux" >> "${OS_RELEASE}"
 echo "IMAGE_VERSION=${IMAGE_VERSION}" >> "${OS_RELEASE}"
+
+# Mount efivarfs to access UEFI variables
+# Remount as read-write as needed
+FSTAB="${TARGET_DIR}/etc/fstab"
+if ! grep -q efivarfs ${FSTAB};then
+  echo "efivarfs /sys/firmware/efi/efivars efivarfs ro,nosuid,nodev,noexec 0 0" >> ${FSTAB}
+fi
