dcff4dff67b2b5fbf7141ff9f4aba7a7213073eb
Abylay Ospan <aospan@sbnb.io>
2025-04-19 18:24:01 -0400

    containers:perception_models env variables

--- containers/perception_models/demo-run.sh
+++ containers/perception_models/demo-run.sh
@@ -1,10 +1,14 @@
+HF_TOKEN=hf_REPLACE_ME
+QUESTION='What is happening in the video?'
+MEDIA='/tmp/demo.mp4'
+
 docker run -it --gpus all \
     -v /tmp:/tmp \
     -v /root/.cache:/root/.cache \
-    -e HF_TOKEN=hf_REPLACE_ME \
+    -e HF_TOKEN="${HF_TOKEN}" \
     sbnb/perceptionlm \
     conda run -n perception_models --no-capture-output \
     python3 /perception_models/apps/plm/generate.py \
     --ckpt facebook/Perception-LM-1B --media_type video\
-    --question 'What is happening in the video?' \
-    --media_path  /tmp/demo.mp4
+    --question "${QUESTION}" \
+    --media_path "${MEDIA}"
